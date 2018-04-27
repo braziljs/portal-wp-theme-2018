e2f563c601beb3115d2ceef9b033fd4f76abe840
Gabriel Corrêa <gabe.crs@gmail.com>
2018-04-27 16:37:24 -0300

    [*]

--- sections/home-products.php
+++ sections/home-products.php
@@ -9,7 +9,7 @@
 		<div class="products-highlight__wrapper">
 			<h3 class="products__name">Eventos</h3>
 			
-			<div class="products-highlight__info">	
+			<div class="products-highlight__info">
 				<div class="products-highlight">
 					<div class="products-highlight__item">
 						<strong class="products-highlight__value">2.000</strong>
@@ -47,74 +47,82 @@
 			</li>
 		</ul>
 	</div>
-	<?php /* ?>
-	<ul class="products__list">
 
-		<li class="product product__type-2">
-			<div class="products__item">
-				<div class="col-5-12 flow-opposite last">
-					<div class="col-4-12">
-						<h3 class="products__name">Conteúdo</h3>
-						<p class="paragraph paragraph--color-2">Levamos conteúdo de qualidade para toda a comunidade. Através da BrazilJS Weekly, artigos no portal, vídeos no Youtube, screencasts, cursos, etc, é possível estar sempre atualizado com o que existe de novo no mundo Front-end, além de se profissionalizar, tudo isso com o selo de qualidade BrazilJS.</p>
+	<div class="products-list__type-2">
+		
+		<div class="product">
+
+			<div class="products-highlight__wrapper">
+				<h3 class="products__name">Conteúdo</h3>
+
+				<div class="products-highlight__info">
+					<div class="products-highlight">
+						<div class="products-highlight__item">
+							<strong class="products-highlight__value"><?php $count_posts = wp_count_posts(); echo $count_posts->publish; ?></strong>
+							<span class="products-highlight__key">Artigos publicados</span>
+						</div>
+						<div class="products-highlight__item">
+							<strong class="products-highlight__value">6K+</strong>
+							<span class="products-highlight__key">Inscritos no Youtube</span>
+						</div>
+					</div>
+					<div class="products-paragraph">
+						<p class="paragraph paragraph--color-1">
+							Levams conteúdo de qualidade para toda a comunidade. através da braziljs weekly, artigos no portal, vídeos no youtube,
+							screencast, cursos e etc.
+						</p>
+						<div class="products-links__wrapper">
+							<ul class="products-links__list">
+								<li class="products-links__item">
+									<a href="<?php bloginfo('url'); ?>/blog/" class="anchor-link">Nossos artigos</a>
+								</li>
+								<li class="products-links__item">
+									<a href="https://www.youtube.com/channel/UCnLdHOuue5i1O7TsH6oh07w" class="anchor-link">Canal do Youtube</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-				<ul class="products-highlight col-4-12">
-					<li class="products-highlight__item col-2-12">
-						<strong class="products-highlight__value"><?php $count_posts = wp_count_posts(); echo $count_posts->publish; ?></strong>
-						<span class="products-highlight__key">Artigos publicados</span>
-					</li>
-					<li class="products-highlight__item col-2-12">
-						<strong class="products-highlight__value">6k +</strong>
-						<span class="products-highlight__key">Inscritos no Youtube</span>
-					</li>
-				</ul>
-				<picture>
-					<img class="products__thumb" src="<?php asset_path('img/products-example--2.jpg') ?>" alt="" />
-				</picture>
-			</div>
-			<ul class="products-links products-links--type-2">
-				<li class="products-links__item">
-					<a href="<?php bloginfo('url'); ?>/blog/" class="products-links__anchor anchor">Veja todos os artigos</a>
-				</li>
-				<li class="products-links__item">
-					<a href="https://www.youtube.com/channel/UCnLdHOuue5i1O7TsH6oh07w" class="products-links__anchor anchor">Confira nosso canal no Youtube</a>
-				</li>
-			</ul>
-		</li>
 
-		<li class="product product__type-3">
-			<div class="products__item flow-opposite">
-				<div class="col-5-12">
-					<div class="col-4-12 flow-opposite">
-						<h3 class="products__name">Comunidade e open-source</h3>
-						<p class="paragraph paragraph--color-2">Sem comunidade, não existe a BrazilJS. Estamos sempre focados em ajudar e fomentar o crescimento saudável da comunidade JavaScript no Brasil. Amamos e acreditamos no Open-source, por isso, sempre que possível, desenvolvemos projetos que possam ajudar outros desenvolvedores.</p>
+			</div>
+		</div>
+
+		<div class="product">
+
+			<div class="products-highlight__wrapper">
+				<h3 class="products__name">Comunidade e open-source</h3>
+
+				<div class="products-highlight__info">
+					<div class="products-highlight">
+						<div class="products-highlight__item">
+							<strong class="products-highlight__value">10K+</strong>
+							<span class="products-highlight__key">Inscritos na weekly</span>
+						</div>
+						<div class="products-highlight__item">
+							<strong class="products-highlight__value">1.5K</strong>
+							<span class="products-highlight__key">Registrados no Slack</span>
+						</div>
+					</div>
+					<div class="products-paragraph">
+						<p class="paragraph paragraph--color-1">
+							Sem comunidade, não existe a braziljs. Amamos e acreditamos no open-source, por isso, sempre que possível, desenvolvemos
+							projetos que possam ajudar outros desenvolvedores.
+						</p>
+						<div class="products-links__wrapper">
+							<ul class="products-links__list">
+								<li class="products-links__item">
+									<a href="https://now-examples-slackin-vvjogtwzcf.now.sh/" class="anchor-link">Canal no Slack</a>
+								</li>
+								<li class="products-links__item">
+									<a href="http://braziljs.us5.list-manage.com/subscribe/post?u=77b6594f10bba05dcc722c80e&id=e6beed4270" class="anchor-link">Assine a Weekly</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
-				<ul class="products-highlight col-4-12 last">
-					<li class="products-highlight__item col-2-12">
-						<strong class="products-highlight__value">10k +</strong>
-						<span class="products-highlight__key">Inscritos na weekly</span>
-					</li>
-					<li class="products-highlight__item col-2-12">
-						<strong class="products-highlight__value">1.5k</strong>
-						<span class="products-highlight__key">Registrados no Slack</span>
-					</li>
-				</ul>
-				<picture>
-					<img class="products__thumb" src="<?php asset_path('img/products-example--3.jpg') ?>" alt="" />
-				</picture>
 			</div>
-			<ul class="products-links">
-				<li class="products-links__item">
-					<a href="https://now-examples-slackin-vvjogtwzcf.now.sh/" class="products-links__anchor anchor">Vá para o nosso canal no Slack</a>
-				</li>
-				<li class="products-links__item">
-					<a href="http://braziljs.us5.list-manage.com/subscribe/post?u=77b6594f10bba05dcc722c80e&id=e6beed4270" class="products-links__anchor anchor">Assine a BrazilJS Weekly</a>
-				</li>
-			</ul>
-		</li>
+		</div>
+	</div>
 
-	</ul>
-	<?php */ ?>
 </div>
 <!-- END HOME PRODUCTS -->
