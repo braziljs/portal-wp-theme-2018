269e690f555c0b0e0f9094c76a1ebdc9eba21ea3
Gabriel Corrêa <gabe.crs@gmail.com>
2018-05-10 17:42:29 -0300

    [+] section hostel in conf page

--- sections/conf/hotels.php
+++ sections/conf/hotels.php
@@ -2,11 +2,14 @@
 <section class="section-wrapper">
 	<div class="content">
 		<div class="hostels-wrapper">
-			<div>
+			<div class="hostel-icon">
+				<img src="<?php asset_path('img/icons/icon-bad.svg') ?>" alt="">
+			</div>
+			<div class="hostel-legends">
 				<strong>Precisando de hotel?</strong>
 				<p>Veja as melhores opções e tarifas de hospedagens para participantes</p>
 			</div>
-			<a class="anchor-link hotels-container__link" href="">ver hospedagens</a>
+			<a class="anchor-button anchor-button--color-5 hotels-container__link" href="">ver hospedagens</a>
 		</div>
 	</div>
 </section>
