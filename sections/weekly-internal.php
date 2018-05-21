b917c7664b4ad64353860d7f5f1f47352f2f0f3c
Gabriel Corrêa <gabe.crs@gmail.com>
2018-05-21 10:07:37 -0300

    [-]

--- sections/weekly-internal.php
+++
@@ -1,5 +0,0 @@
-<!-- WEEKLY NEWSLETTER INTERNAL VERSION -->
-<section class="newsletter-wrapper">
-	<?php get_template_part('sections/weekly-content'); ?>
-</section>
-<!-- END WEEKLY NEWSLETTER INTERNAL VERSION -->
